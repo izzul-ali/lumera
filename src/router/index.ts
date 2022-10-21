dec3c907f6d2d09d435f2777544d2ae9ccb2cedd
izzul-ali <izzulmaali3@gmail.com>
2022-10-21 10:21:53 +0700

    fix lazy load component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from '../views/home.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/', name: 'Home', component: () => import('../views/home.vue') },
-        {path: '/about',name: 'About',component: () => import('../views/about.vue')},
-        {path: '/:pathmatch(.*)*',name: 'NotFound',component: () => import('../views/404.vue')}
+        { path: '/', name: 'Home', component: Home },
+        { path: '/contact', name: 'Contact', component: () => import('../views/contact.vue') },
+        { path: '/:pathmatch(.*)*', name: 'NotFound', component: () => import('../views/404.vue') }
     ]
-}) 
+})
 
 export default router
